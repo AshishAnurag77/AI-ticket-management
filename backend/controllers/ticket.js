551abc76dbdd5c372cbbb0907c7177779917de94
Ashish Anurag Barik <156515171+AshishAnurag77@users.noreply.github.com>
2025-07-12 19:53:49 +0530

    Updated ticket.js

--- backend/controllers/ticket.js
+++ backend/controllers/ticket.js
@@ -1,19 +1,7 @@
-let ticket;
-
-    if (user.role !== "user") {
-      ticket = await Ticket.findById(req.params.id).populate("assignedTo", [
-        "email",
-        "_id",
-      ]);
-    } else {
-      ticket = await Ticket.findOne({
-        createdBy: user._id,
-        _id: req.params.id,
-      }).select("title description status createdAt");
-    }
-
-    if (!ticket) {
-      return res.status(404).json({ message: "Ticket not found" });
-    }
-    return res.status(200).json({ ticket });
-  } catch (error) {
+let tickets = [];
+if (user.role !== "user") {
+  tickets = await Ticket.find({})
+    .populate("assignedTo", ["email", "_id"])
+    .sort({ createdAt: -1 });
+} else {
+}
